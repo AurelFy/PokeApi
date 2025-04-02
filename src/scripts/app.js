b5ae6e77b6c173071e672219505652e400e152ea
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-04-02 15:53:35 +0200

    base html + css

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,12 @@
-console.info('Hello world');
+'use strict'; 
+
+var inputText = document.querySelector('.search--input');
+var btn = document.querySelector('.search--btn');
+
+btn.addEventListener('click', function () {
+    var text = 'La valeur du champ est : ' + inputText.value; 
+    console.log(text);
+    // créer l'url qui correspond à la valeur du champs
+    var url
+    // faire un fetch pour avoir les données json
+});
